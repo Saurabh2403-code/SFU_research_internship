7b4e33cf04e30001d4c613b1a50934d189df0f64
Saurabh Giri <girisaurabh2020@gmail.com>
2026-07-30 09:54:19 -0600

    Changing the format to save the distance matrix

--- examples/images/cifar10/Analysis.py
+++ examples/images/cifar10/Analysis.py
@@ -6,6 +6,7 @@ from torchvision import datasets,transforms
 from absl import app,flags
 from tqdm import trange
 import numpy as np
+import matplotlib.pyplot as plt
 parent_folder= os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(parent_folder)
 from utils_cifar import *
@@ -16,7 +17,7 @@ from torchcfm.conditional_flow_matching import (
     VariancePreservingConditionalFlowMatcher,
 )
 from torchcfm.models.unet.unet import UNetModelWrapper
-device=torch.device('cuda') if torch.cuda.is_available else 'cpu'
+device=torch.device('cuda') if torch.cuda.is_available() else 'cpu'
 FLAGS=flags.FLAGS
 flags.DEFINE_integer('num_images',100,help='Number Of images to generate')
 flags.DEFINE_integer('num_channels',128,help='Number Of Base Colour Channels')
@@ -42,6 +43,8 @@ def inference(argv):
     model_dir=FLAGS.output_dir+f'{FLAGS.model}'+'/'
     save_dir=model_dir+'distances'+'/'
     os.makedirs(save_dir,exist_ok=True)
+    heatmap_dir=save_dir+f'{FLAGS.model}_{FLAGS.num_images}'
+    os.makedirs(heatmap_dir,exist_ok=True)
     state_dict=torch.load(model_dir+f'{FLAGS.model}_cifar10_weights_step_{FLAGS.step}.pt', weights_only=True)
     net_model.load_state_dict(state_dict["net_model"])
     net_model.eval()
@@ -49,9 +52,11 @@ def inference(argv):
         generated_samples=generate_samples(net_model, FLAGS.parallel,model_dir+f"{FLAGS.model}_cifar10_weights_step_{FLAGS.step}.pt",step=FLAGS.step,time_steps=FLAGS.time_steps,number_of_images=FLAGS.num_images,net_="normal")
         original_dataset=get_original_image(FLAGS.num_images)
         l2_distance=get_l2_distance(generated_samples,original_dataset)
-        with open(save_dir+f'{FLAGS.model}_Distances_{FLAGS.num_images}.txt','a') as f:
-            f.write(str(l2_distance.tolist()))
-        plt.savefig(save_dir+f'{FLAGS.model}_{FLAGS.num_images}/Distances_Heat Map.png')
+        torch.save(l2_distance,save_dir+f'{FLAGS.model}_Distances_{FLAGS.num_images}.pt')
+        plt.imshow(l2_distance.detach().cpu().numpy(),origin='lower')
+        plt.colorbar()
+        plt.savefig(os.path.join(heatmap_dir,'Distances_Heat_Map.png'),bbox_inches='tight')
+        plt.close()
 if __name__=="__main__":
     app.run(inference)
 
